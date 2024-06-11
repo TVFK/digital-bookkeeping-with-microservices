d37de6d017f7d64ecd26f6207a524bffc18e6873
Tavfik <tavfik2408@gmail.com>
2024-06-11 15:21:02 +0300

    initial commit

---
+++ src/main/java/ru/taf/digital_bookkeeping/Project7DigitalBookkeepingWithMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package ru.taf.digital_bookkeeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project7DigitalBookkeepingWithMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project7DigitalBookkeepingWithMicroservicesApplication.class, args);
+	}
+
+}
